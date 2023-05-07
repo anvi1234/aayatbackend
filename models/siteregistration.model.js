08e0892687ae64759361c7959459f8ce15df8179
Anveshika <anveshika.srivastava@epsilonium.com>
2023-05-07 09:27:48 +0530

    first commit

---
+++ models/siteregistration.model.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var SiteRegistrationSchema = new mongoose.Schema({
+    id:String,
+    siteName:String,
+    location:String,
+    work:String,
+    number:String,
+    poNo:String,
+    billNo:String,
+    date:Date,
+    status:String,
+    closingdate:Date,
+    uniqueSiteId:String
+})
+
+module.exports = mongoose.model('SiteRegistration',SiteRegistrationSchema);
