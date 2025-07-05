2ef45841901d5abb77e23db46faa0d4930794c0d
Anveshika <anveshika.srivastava@epsilonium.com>
2025-07-05 17:28:06 +0530

    add staus code

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,35 +3,47 @@ const passport = require('passport');
 const _= require('lodash')
 const User = mongoose.model('User')
 
-module.exports.register = (req,res,next) =>{
-    var user = new User()
-    user.siteName = req.body.siteName;
-    user.location = req.body.location;
-    user.fullName = req.body.fullName;
-    user.email = req.body.email;
-    user.password = req.body.password;
-    user.designation = req.body.designation;
-    user.mobileNo = req.body.mobileNo;
-    user.adharNo = req.body.adharNo;
-    user.address = req.body.address;
-    user.basicPay = req.body.basicPay;
-    user.bankName = req.body.bankName;
-    user.accNo = req.body.accNo;
-    user.ifsccode = req.body.ifsccode;
-    user.uniqueSiteId = req.body.uniqueSiteId;
-    user.save((err,doc)=>{
-        if(!err)
-        res.send(doc);
-        else{
-            if(err.code==11000){
-                return res.status(400).send({ message: "Email already exists." });
-            }
-            else{
-                return next(err);
+module.exports.register = (req, res, next) => {
+    const user = new User({
+        siteName: req.body.siteName,
+        location: req.body.location,
+        fullName: req.body.fullName,
+        email: req.body.email,
+        password: req.body.password,
+        designation: req.body.designation,
+        mobileNo: req.body.mobileNo,
+        adharNo: req.body.adharNo,
+        address: req.body.address,
+        basicPay: req.body.basicPay,
+        bankName: req.body.bankName,
+        accNo: req.body.accNo,
+        ifsccode: req.body.ifsccode,
+        uniqueSiteId: req.body.uniqueSiteId,
+    });
+
+    user.save((err, doc) => {
+        if (!err) {
+            return res.status(200).send({
+                message: "User registered successfully",
+                data: doc,
+                status:200
+            });
+        } else {
+            if (err.code === 11000) {
+                return res.status(400).send({
+                    message: "Email already exists.",
+                    status:400
+                });
+            } else {
+                return res.status(500).send({
+                    message: "Something went wrong.",
+                    error: err.message,
+                    status:500
+                });
             }
         }
-    })
-}
+    });
+};
 
 module.exports.authenticate=(req,res,next)=>{
 passport.authenticate('local',(err,user,info)=>{
