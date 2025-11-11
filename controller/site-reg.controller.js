2d687bf4c61a5b90645aca7b0f0de633d2937c98
Anveshika <anveshika.srivastava@epsilonium.com>
2025-11-11 11:29:48 +0530

    fixed issue

--- controller/site-reg.controller.js
+++ controller/site-reg.controller.js
@@ -3,7 +3,8 @@ const passport = require('passport');
 const _= require('lodash');
 const { isEmpty } = require('lodash');
 const SiteRegistration = mongoose.model('SiteRegistration')
-const Expenses =  mongoose.model('Expense')
+const Expenses =  mongoose.model('Expense');
+const Transaction = mongoose.model('Transaction');
 
 module.exports.addSite = (req,res,next) =>{
     var  siteRegistration = new  SiteRegistration()
@@ -96,3 +97,58 @@ module.exports.getSiteReg = (req,res,next)=>{
         });
       }
  
+module.exports.getSiteSummary = async (req, res, next) => {
+  try {
+    // Step 1: Get all sites
+    const sites = await SiteRegistration.find();
+
+    // Step 2: Build summary for each site
+    const siteSummaries = await Promise.all(
+      sites.map(async (site) => {
+        const { siteName, location, uniqueSiteId } = site;
+
+        // Calculate total expense for that site
+        const expenseTotalResult = await Expenses.aggregate([
+          { $match: { uniqueSiteId } },
+          { $group: { _id: null, totalExpense: { $sum: "$expenseAmount" } } }
+        ]);
+
+        const totalExpenseAmount =
+          expenseTotalResult.length > 0
+            ? expenseTotalResult[0].totalExpense
+            : 0;
+
+        // Calculate total transaction for that site
+        const transactionTotalResult = await Transaction.aggregate([
+          { $match: { uniqueSiteId } },
+          { $group: { _id: null, totalTransaction: { $sum: "$totalAmount" } } }
+        ]);
+
+        const totalTransactionAmount =
+          transactionTotalResult.length > 0
+            ? transactionTotalResult[0].totalTransaction
+            : 0;
+
+        return {
+          siteName,
+          location,
+          date: site.date || null,
+          totalExpenseAmount,
+          totalTransactionAmount
+        };
+      })
+    );
+
+    res.status(200).json({
+      success: true,
+      data: siteSummaries
+    });
+  } catch (error) {
+    console.error("Error fetching site summary:", error);
+    res.status(500).json({
+      success: false,
+      message: "Error fetching site summary",
+      error: error.message
+    });
+  }
+};
